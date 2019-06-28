b5a9d18a69847a0d661bea8d93c305a1027c5de2
Ubuntu <ubuntu@ip-172-31-94-82.ec2.internal>
2019-06-28 22:53:24 +0000

    after challenge 4 traversing sideways

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// $(".stream-nav").on("click", function() {
-//     // A selector to match all cards in all streams
-//     var allStreamsCardsSelector = ".card";
-//     // A selector to match just this stream's cards
-//     // for this, we use the class with the name of the stream,
-//     // which is the same as this nav link's id and then the "-card" suffix.
-//     var thisStreamCardsSelector = "." + this.id + ".card";
-
-//     // First remove the highlight from all of the cards
-//     $(allStreamsCardsSelector).removeClass("card-highlight");
-//     // Then apply the highlight to just this stream's cards
-//     $(thisStreamCardsSelector).addClass("card-highlight");
-// });
-
 $(document).ready(function() {
     // $('p').click(function() {
     //     $('p').children('a').css('background-color', 'yellow');
@@ -26,5 +12,13 @@ $(document).ready(function() {
     // });
     $('.card').click(function() {
         $(this).removeClass('card_white').toggleClass('highlight');
+    });
+
+    $('#select_btn').click(function(){
+       $('.card:not(.highlight)').hide(); 
+    });
+    //select all cards
+    $('#all_btn').click(function() {
+        $('.card').show();
     });
 });
