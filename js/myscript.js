dd459a3c31ea6297efcc774ad32c40a3a361e8d7
rory81 <nbleijie.gmail.com>
2019-06-29 00:19:44 +0000

    after challenge2 other traversing methods

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $("tr:even").addClass("evenColor");
     $("tr:odd").addClass("oddColor");
+
+    $("th").click(function() {
+       $('tr').children().removeClass('highlight');
+       $(this).siblings().addClass('highlight');
+    });
 });
